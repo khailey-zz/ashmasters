fba346581a5ba3a5de44b9830fe49729c7e84b3b
Kyle Hailey <kylelf@gmail.com>
2012-11-07 10:56:06 -0800

    added ash_io_top_obj.sql

---
+++ ash_io_top_obj.sql
@@ -0,0 +1,20 @@
+
+col objn for a28
+
+
+select
+       count(*) cnt,
+       CURRENT_OBJ#||' '||o.object_name objn,
+       o.object_type otype
+from v$active_session_history ash,
+      all_objects o
+where ( event like 'db file s%' or event like 'direct%' )
+   and o.object_id (+)= ash.CURRENT_OBJ#
+   and sample_time > sysdate - &1/(60*24)
+   and session_state='WAITING'
+group by
+       CURRENT_OBJ#, o.object_name ,
+       o.object_type
+Order by count(*)
+/
+
